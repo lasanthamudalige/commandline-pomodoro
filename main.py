24a6bdbbfc3053667d0f2ce7af74e56355b4eb52
lasantha <lasantha1130@gmail.com>
2023-11-27 13:19:03 +0530

    add 30 min notification message and update 5 minutes break message

--- main.py
+++ main.py
@@ -26,10 +26,11 @@ def main():
         if countdown == 0 and not on_break:
             # Get 30 minutes break after 4, 10 minutes breaks.
             if break_amount == 4:
+                send_notification("Excellent!", "Take a 30 minutes break now.")
                 countdown = 60 * 30
                 break_amount = 0
             else:
-                send_notification("well done!", "Take a 10 minutes break now.")
+                send_notification("well done!", "Take a 5 minutes break now.")
                 on_break = True
                 countdown = break_time
                 break_amount += 1
